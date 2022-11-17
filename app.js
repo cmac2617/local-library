6d15dc3847b743c46300b3185e382110370794f4
Sile <cmac2617@yahoo.com>
2022-11-16 22:33:38 -0500

    Added files and changes required for deployment.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://American:12boom13@cluster0.wifbd7r.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://American:12boom13@cluster0.wifbd7r.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
